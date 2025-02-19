8c60a887d7349db2201f6f716c0d10200022d67b
amaykorade <amaykorade5@gmail.com>
2025-02-20 00:51:34 +0530

    Solved the data sending part with authentication where userId is sent in query rather than in headers

--- frontend/frontend/src/components/TopPages.jsx
+++ frontend/frontend/src/components/TopPages.jsx
@@ -18,9 +18,19 @@ export default function TopPages() {
 
   // Function to format time in MM:SS
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor(seconds / 3600 / 60);
     const remainingSeconds = Math.round(seconds % 60);
-    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+
+    if (hours > 0) {
+      return `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${
+        remainingSeconds < 10 ? "0" : ""
+      }${remainingSeconds}`;
+    } else {
+      return `${minutes}:${
+        remainingSeconds < 10 ? "0" : ""
+      }${remainingSeconds}`;
+    }
   };
 
   return (
