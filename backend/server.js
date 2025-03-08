30f35db6e271a20d79dbfe9290c1f0c94bba2287
amaykorade <amaykorade5@gmail.com>
2025-03-08 13:06:25 +0530

    added the simple get route

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 let allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3001',
+    'https://www.webmeter.in/',
     // 'http://127.0.0.1:5500'
     // 'https://persona-website.onrender.com/',
 ];
