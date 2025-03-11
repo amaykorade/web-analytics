c14e51095eb8ace65ea580bb8fd0b1cf8286dead
amaykorade <amaykorade5@gmail.com>
2025-03-11 20:07:07 +0530

    All the latest changes into the main

--- frontend/frontend/src/services/apiClient.js
+++ frontend/frontend/src/services/apiClient.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const apiClient = axios.create({
     // baseURL: "http://localhost:3000/api",
     // baseURL: "http://localhost:3000/api",
-    baseURL: "https://backend.webmeter.in",
+    // baseURL: "https://backend.webmeter.in",
+    baseURL: "https://backend.webmeter.in/api",
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
