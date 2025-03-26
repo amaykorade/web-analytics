39eaeb2cb2d7cc65355eaac5c8221a405d2087b5
amaykorade <amaykorade5@gmail.com>
2025-03-26 15:44:06 +0530

    Update signup and login ui

--- frontend/frontend/src/components/auth/login.jsx
+++ frontend/frontend/src/components/auth/login.jsx
@@ -202,7 +202,7 @@ export default function Login() {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Create account
+                Login
               </button>
             </div>
           </form>
