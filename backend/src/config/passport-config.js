af53a402759824796d680c6ba068a2bbd39ab063
amaykorade <amaykorade5@gmail.com>
2025-04-02 17:26:48 +0530

    Added signup and login using google update 1

--- backend/src/config/passport-config.js
+++ backend/src/config/passport-config.js
@@ -12,7 +12,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "https://www.webmeter.in/api/user/google/callback",
+            callbackURL: "https://backend.webmeter.in/api/user/google/callback",
             passReqToCallback: true,
         },
         async (req, accessToken, refreshToken, profile, done) => {
